96771544dd8c6abe150d09a5382e48cc51b2c2ea
Thales <thalesondas0@gmail.com>
2026-09-11 13:39:58 -0300

    feat: add experiment validation and error handling

--- src/experiments/dto/create-experiment.dto.ts
+++ src/experiments/dto/create-experiment.dto.ts
@@ -1,4 +1,16 @@
+import { IsEnum, IsNotEmpty, IsString, MaxLength } from "class-validator"
+import { ExperimentStatus } from "../enums/experiment-status.enum.js";
+
 export class CreateExperimentDto {
-    name: string
-    status: string
+    @IsNotEmpty({ message: 'O nome não deve estar vazio.' })
+    @IsString({ message: 'O nome deve ser um texto.' })
+    @MaxLength(32, { message: 'O nome deve ter no máximo 32 caracteres.' })
+    name: string;
+    
+    @IsNotEmpty({ message: 'O status não deve estar vazio.' })
+    @IsEnum(
+        ExperimentStatus,
+        { message: `O status deve ser pending, running, completed ou failed.` }
+    )
+    status: ExperimentStatus;
 }
---
+++ src/experiments/enums/experiment-status.enum.ts
@@ -0,0 +1,6 @@
+export enum ExperimentStatus {
+    PENDING = 'pending',
+    RUNNING = 'running',
+    COMPLETED = 'completed',
+    FAILED = 'failed',
+}
